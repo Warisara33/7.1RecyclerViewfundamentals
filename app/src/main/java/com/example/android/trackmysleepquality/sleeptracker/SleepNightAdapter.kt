66919c859720b9e2a62444938029060188e42907
Warisara33 <56625430+Warisara33@users.noreply.github.com>
2020-09-04 15:14:01 +0700

    modify7.1-5step2

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -34,9 +34,9 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
                 .inflate(R.layout.text_item_view, parent, false) as TextView
         return TextItemViewHolder(view)
     }
-}
-class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-    val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-    val quality: TextView = itemView.findViewById(R.id.quality_string)
-    val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
+        val quality: TextView = itemView.findViewById(R.id.quality_string)
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+    }
 }
